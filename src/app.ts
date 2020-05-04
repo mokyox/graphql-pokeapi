b35179c157f16137ef3db5870665a78d29c7a125
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-05-04 16:19:12 +0100

    fix imports with schema/resolvers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { typeDefs } from "../src/graphql/schema/";
-import { resolvers } from "../src/graphql/resolvers/";
+import { typeDefs } from "./graphql/schema";
+import { resolvers } from "./graphql/resolvers";
 
 const server = new ApolloServer({
   typeDefs,

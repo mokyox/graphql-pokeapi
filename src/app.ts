70c462dc96b3c071fe8700efc4bb45a278b91803
Mo Jama <23492964+mokyox@users.noreply.github.com>
2020-05-04 14:50:14 +0100

    add introspection and playground to production

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,12 @@ import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "../src/graphql/schema/";
 import { resolvers } from "../src/graphql/resolvers/";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 const port = process.env.PORT || 4000;
 const app = express();

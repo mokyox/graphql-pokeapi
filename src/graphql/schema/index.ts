f4d874e7ba08679f2b14ba83e3cc1251c9faef90
Mo Jama <23492964+mokyox@users.noreply.github.com>
2021-08-13 21:22:30 +0100

    refactor: refactor app to use axios. This commit also ensures that pokedex entries for Pokemon that have Gen 8 (Pokemon Sword/Shield) are shown instead of Alpha Sapphire (Gen 6). The PokeAPI has some missing entries and some pokemon don't have Pokedex entries for Gen 8, so we're falling back to Gen 6.

--- src/graphql/schema/index.ts
+++ src/graphql/schema/index.ts
@@ -7,46 +7,45 @@ export const typeDefs = gql`
   }
 
   type Pokemon {
-    id: Int
-    name: String
+    id: Int!
+    name: String!
     abilities: [AbilityData]
     stats: [StatsData]
     description: [Description]
   }
 
   type Ability {
-    name: String
-    url: String
+    name: String!
+    url: String!
   }
 
   type AbilityData {
     ability: Ability
-    is_hidden: Boolean!
   }
 
   type Stats {
-    name: String
-    url: String
+    name: String!
+    url: String!
   }
 
   type StatsData {
-    stat: Stats
-    base_stat: Int
+    stat: Stats!
+    base_stat: Int!
   }
 
   type Description {
-    flavor_text: String
-    language: LanguageData
-    version: VersionData
+    flavor_text: String!
+    language: LanguageData!
+    version: VersionData!
   }
 
   type LanguageData {
-    name: String
-    url: String
+    name: String!
+    url: String!
   }
 
   type VersionData {
-    name: String
-    url: String
+    name: String!
+    url: String!
   }
 `;
